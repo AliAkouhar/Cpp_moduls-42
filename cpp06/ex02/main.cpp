ac7dbb8686c53fe9e4993f3b209f304805172670
AliAkouhar <akouhar06ali@gmail.com>
2025-07-15 09:15:28 +0100

    push

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,12 @@
+#include "utils.hpp"
+
+int main()
+{
+    Base* ptr = generate();
+    std::cout << "Identify by pointer:" << std::endl;
+    idnetify(ptr);
+    std::cout << "Identify by reference:" << std::endl;
+    identify(*ptr);
+    delete ptr;
+    return 0;
+}
---
+++ cpp06/ex02/utils.cpp
@@ -0,0 +1,58 @@
+#include "B.hpp"
+#include "A.hpp"
+#include "C.hpp"
+#include "Base.hpp"
+
+Base* generate(void)
+{
+    int i;
+    srand(time(NULL));
+    i = rand();
+    if (i % 2 == 0)
+        return new A();
+    else if (i % 3 == 0)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p) != NULL)
+        std::cout << "The type of the object is A" << std::endl;
+    else if (dynamic_cast<B*>(p) != NULL)
+        std::cout << "The type of the object is B" << std::endl;
+    else if (dynamic_cast<C*>(p) != NULL)
+        std::cout << "The type of the object is C" << std::endl;
+    else
+        std::cout << "Unknown type" << std::endl;
+}
+
+void idnetify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "The type of the object is A" << std::endl;
+        return;
+    }
+    catch (const std::exception& e)
+    {
+    }
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "The type of the object is B" << std::endl;
+        return;
+    }
+    catch (const std::exception& e)
+    {
+    }
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "The type of the object is C" << std::endl;
+        return;
+    }
+    catch (const std::exception& e)
+    {
+    }
+    std::cout << "Unkown type" << std::endl;   
+}
